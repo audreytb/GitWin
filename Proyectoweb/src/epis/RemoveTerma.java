376b1f95045272149f2c1d5fb1c1b87e4aa9b3df
Audrey <audreytacca@gmail.com>
2015-06-24 23:02:54 -0500

    TermaRemove
    Lo salvo

--- Proyectoweb/src/epis/RemoveTerma.java
+++ Proyectoweb/src/epis/RemoveTerma.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,21 +22,16 @@ import javax.servlet.http.HttpServletResponse;
 			int posicion=Integer.parseInt(dato);
 			final PersistenceManager pm = PMF.get().getPersistenceManager();
 			Query q = pm.newQuery(Terma.class);
-			
+			Terma t;
 				try{
 					List<Terma> termas = (List<Terma>) q.execute();
-				
-					for(Terma t: termas){
-						String y=t.get(posicion-1);
-						pm.deletePersistent(y);
-					}
-
-					resp.getWriter().println("Se han borrado todos los Tutoriales.");
-					resp.sendRedirect("/tutorial?action=show");
+						t=termas.get(posicion-1);
+						pm.deletePersistent(t);
+						req.setAttribute("termas", termas);
+						RequestDispatcher rd = getServletContext().getRequestDispatcher("/termaAll.jsp");
+						rd.forward(req, resp);
 				}catch(Exception e){
 						System.out.println(e);
-						resp.getWriter().println("No se han podido borrar todas personas.");
-						resp.sendRedirect("/tutorial?action=show");
 				}finally{
 					q.closeAll();
 					pm.close();
